748726b3be1f4d14a1ccecb3dd2c9e13bb80e08d
devannugrahahp <devannhp089@gmail.com>
2025-05-20 08:40:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("selamat datang di website deple kece banget")
+st.title("selamat datang di website devan kece banget")
 st.write(
     "ngoding seru bersama deple kece"
 )
