e1dc09241757b296deab0c565f0852a743642e21
devannugrahahp <devannhp089@gmail.com>
2025-05-27 07:59:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,15 @@ st.write(
     "devan deple kece XF"
 )
 st.image("IMG-20250520-WA0020.jpg", width=500)
+
+
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
+    
